ea22a912f4b6155b265e06d86a2cca00b5bbea22
Utkarsh13124 <utkarshp579@gmail.com>
2026-02-19 21:38:47 +0530

    LLD Parking Lot Practice Session - 1

---
+++ SystemDesignPractice/ParkingLot.java
@@ -0,0 +1,18 @@
+package SystemDesignPractice;
+
+public class ParkingLot {
+    
+}
+
+
+// RequireMent 
+    // Limited Area --> Multifloor
+    // Entry and exit points
+    // ticket collection and payment
+    // payment [ Cash , Card]
+    // capacity management
+    // Parking Spots
+    // parking fee model
+
+
+// Use Cases
